31f7f72044cfc4183af926036342c48353d9af06
e-losiev <lowk3y.loko@gmail.com>
2024-11-28 16:40:15 +0200

    clean up

--- contracts/lib/constants.sol
+++ contracts/lib/constants.sol
@@ -5,8 +5,6 @@ pragma solidity ^0.8.26;
 address constant E280 = 0xe9A53C43a0B58706e67341C4055de861e29Ee943;
 address constant DRAGONX = 0x96a5399D07896f757Bd4c6eF56461F58DB951862;
 address constant SCALE = 0xEADeB14bea62B136Ff0D486937aEf9BEe4759c6A;
-
-// ===================== UNISWAP Interface ======================================
-address constant UNISWAP_V2_FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
 address constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-uint32 constant BPS_BASE = 10_000;
+
+uint32 constant BPS_BASE = 100_00;
